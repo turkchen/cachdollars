00ee0dba37020a2cd4fd61d43355b2dc2d20bb87
Serdar NAZLI <nazliserdar@icloud.com>
2021-09-07 16:02:26 +0300

    son degisiklik

--- app/src/main/java/com/edsn/cachdollars/MainActivity.java
+++ app/src/main/java/com/edsn/cachdollars/MainActivity.java
@@ -1,16 +1,37 @@
 package com.edsn.cachdollars;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    SharedPreferences sharedPreferences;
+    int maxRecord;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("BEBISIMMMMM");
-        System.out.println("amsdmasmdas");
-        System.out.println("askimmmmm");
+        sharedPreferences = this.getSharedPreferences("com.edsn.cachdollars", Context.MODE_PRIVATE);
+        maxRecord = sharedPreferences.getInt("maxRecord",0);
+
+
+
+
+
     }
+
+    public void play(View view){
+        Intent intent = new Intent(MainActivity.this,PlayingActivity.class);
+        intent.putExtra("maxRecord",maxRecord)
+        startActivity(intent);
+
+    }
+
 }
