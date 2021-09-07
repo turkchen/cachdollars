ccb089c4622ed73fb173722a64af3c0961e89178
Serdar NAZLI <nazliserdar@icloud.com>
2021-09-07 16:10:38 +0300

    1.ekran kodu

--- app/src/main/java/com/edsn/cachdollars/MainActivity.java
+++ app/src/main/java/com/edsn/cachdollars/MainActivity.java
@@ -1,16 +1,39 @@
 package com.edsn.cachdollars;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    SharedPreferences sharedPreferences;
+    TextView maxView;
+    int maxRecord;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("BEBISIMMMMM");
-        System.out.println("amsdmasmdas");
-        System.out.println("askimmmmm");
+        sharedPreferences = this.getSharedPreferences("com.edsn.cachdollars", Context.MODE_PRIVATE);
+        maxRecord = sharedPreferences.getInt("maxRecord",0);
+        maxView.setText("Max Record: " + maxRecord);
+
+
+
+    }
+
+    public void play(View view) {
+        Intent intent = new Intent(MainActivity.this, PlayingActivity.class);
+        intent.putExtra("maxRecord",maxRecord);
+        startActivity(intent);
+
+
     }
 }
